9de43b5b63faec9660121e10044b58dcaed7e3e7
dxdunion7 <dxdunion7@gmail.com>
2023-02-28 01:52:18 +0000

    allowed host updated.

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://www.renevaultcapital.com', 'https://127.0.0.1:8000/'] 
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
